1331315861081356e1a44cf76ad48219a86cbc9b
Andi Hermanto <ahermanto47@gmail.com>
2021-12-14 09:56:27 -0500

    Timer To Kafka

---
+++ src/main/java/org/southforestlab/integration/TimerToKafkaRouter.java
@@ -0,0 +1,22 @@
+package org.southforestlab.integration;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TimerToKafkaRouter extends RouteBuilder {
+  
+	
+    @Override
+    public void configure() throws Exception {
+
+
+    	from("timer:foo?period={{myPeriod}}").routeId("timer")
+    	    .setBody().simple("Hello")
+      	    .to("kafka:{{producer.topic}}?brokers={{kafka.host}}:{{kafka.port}}");
+
+    }
+
+}
+
